e91768a258c32bd88e9ff024472c24fbe40f328d
ally-petitt <allypetitt@gmail.com>
2023-04-14 18:01:08 -0700

    added functionality to display cpu details

--- popup/popup.js
+++ popup/popup.js
@@ -1,25 +1,58 @@
 import { CollectData } from '../scripts/collect_data.js'
 
-console.log("test")
-let state = new Object
-const cd = new CollectData(state);
 
-window.onload = () => {
-    setEventListeners()
-}
+const cd = new CollectData();
+
+window.onload = () => { setEventListeners() }
 
 
 function setEventListeners() {
     const cpuInfo = document.getElementById('cpuInfo');
 
-    cpuInfo.addEventListener("click", (e) => {toggleButton(e); cd.getCPUInfo()})
+    cpuInfo.addEventListener("click", handleBtnClick)
 }
 
-function toggleButton(e) {
-    const state = e.target.innerText
+
+async function handleBtnClick(e) {
+    toggleButton(e);
+
+    const outputEl = document.getElementById('output')
+    let info 
+
+    switch(e.target.id){
+
+        case 'cpuInfo': info = await cd.toggleCPUInfo()
+        
+    }
     
-    if (state === "Enabled") e.target.innerText = 'Disabled'
-    else if (state == "Disabled") e.target.innerText = 'Enabled'
+
+    if (info) {
+        
+        outputEl.innerText = JSON.stringify(info)
+        return
+    }
+    
+    outputEl.innerText = ""
+    
+}
+
+
+function toggleButton(e) {
+        
+    // remove the classname "show" if it already has it and update the text
+    if (e.target.classList.contains("show")) {
+        e.target.innerText = 'Hidden'
+        e.target.classList.remove('show')
+        return
+    }
+
+    e.target.innerText = 'Shown'
+
+    // Make sure that only one element hass the classname "show"
+    Array.from(document.getElementsByClassName('show')).forEach((el) => {
+        el.classList.remove('show')
+    })
+    e.target.classList.add('show')
 
 
 }
--- scripts/collect_data.js
+++ scripts/collect_data.js
@@ -1,11 +1,26 @@
 class CollectData {
-    constructor(state) {
+    constructor(state = new Object) {
         this.state = state
-      }    
+        this.state.activeData = "test"
+    }    
 
-	getCPUInfo(){
-		console.log("hello world")		
+	async toggleCPUInfo(){
+        this.state.cpuInfo = !this.state.cpuInfo
+
+
+        if (!this.state.cpuInfo){
+            this.state.activeData = null
+
+        } else {
+
+            const info = await chrome.system.cpu.getInfo()
+            this.state.activeData = info
+        }
+
+
+        return this.state.activeData
 	}
+
 
 }
 
